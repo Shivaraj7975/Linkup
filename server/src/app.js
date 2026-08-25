03e304df19d32a9413b6848985ea1423f1acd034
SHIVARAJ SHEGUNASI <shivarajas79757@gmail.com>
2026-08-25 23:24:17 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const adminRoutes = require('./routes/adminRoutes');
 const { errorHandler, notFoundHandler } = require('./middleware/errorHandler');
 
 const app = express();
-
+app.set('trust proxy', 1);
 // Disable x-powered-by banner
 app.disable('x-powered-by');
 
